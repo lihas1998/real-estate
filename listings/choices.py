d86dc098aa645058d8c984d2e344af3b9128f059
lihas1998 <sahilmanandhar1998@gmail.com>
2021-06-26 21:25:03 +0545

    First commits

---
+++ listings/choices.py
@@ -0,0 +1,65 @@
+bedroom_choices={
+
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10,
+}
+
+price_choices={
+    '10000000':'Rs1,00,00,000',
+    '20000000':'Rs2,00,00,000',
+    '30000000':'Rs3,00,00,000',
+    '40000000':'Rs4,00,00,000',
+    '50000000':'Rs5,00,00,000',
+    '70000000':'Rs7,00,00,000',
+    '60000000':'Rs6,00,00,000',
+    '80000000':'Rs8,00,00,000',
+    '90000000':'Rs9,00,00,000',
+    '90000000':'Rs9crore+',
+
+}
+
+state_choices={
+    'AK':'Province No. 1',
+    'AL':'Province No. 2',
+    'AK':'Bagmati Province',
+    'AS':'Gandaki Province',
+    'AZ':'Lumbini Province',
+    'CA':'Karnali Province',
+    'CO':'Sudurpashchim Province',
+    # 'CT':'Connecticut',
+    # 'DC':'District of COlumbia',
+    # 'DE':'Delaware',
+    # 'FL':'Florida',
+    # 'GA':'Georgia',
+    # 'GU':'Guam',
+    # 'HI':'Hawaii',
+    # 'IA':'Idaho',
+    # 'ID':'District of COlumbia',
+    # 'IL':'Illinois',
+    # 'IN':'Indiana',
+    # 'KS':'Kansas',
+    # 'KY':'Kentucky',
+    # 'LA':'Louisiana',
+    # 'MA':'Massachusetts',
+    # 'MD':'Maryland',
+    # 'ME':'Maine',
+    # 'MI':'Michigan',
+    # 'NM':'New Mexico',
+    # 'NV':'Nevada',
+    
+
+}
+
+
+
+
+
+
